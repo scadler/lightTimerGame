0f3bba6ec7e9e244ed7b2a629e26efdafd50403c
scadler <scadler@s.sfusd.edu>
2020-02-11 04:30:17 +0000

    equation tweaked and delay function works

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var i = 0;
-while(i < 23){
+function switchLight(i){
+    i = i%23;
     i = i+1
-    var x = i
-    x = Math.round(((((x-12)*(x-12))/48)+1)*150)
-    console.log(x+" "+i)
+    var x = Number(Math.round((((((i-12)*(i-12))/48)+1)*400)-325))
+        setTimeout(() => {  console.log(x+" "+i); switchLight(i)}, x);
 }
+var i = 0
+var x = 1000
+switchLight(i)
