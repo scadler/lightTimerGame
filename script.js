7813af8f105d843bc3169d14ccde203cd20d038d
scadler <scadler@s.sfusd.edu>
2020-02-11 04:48:56 +0000

    tweaked timing

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ function switchLight(i){
     i = i%23;
     i = i+1
     var x = Number(Math.round((((((i-12)*(i-12))/48)+1)*400)-325))
+    if(x>500){
+        setTimeout(() => {  console.log(x+" "+i); switchLight(i)}, 500);
+    }
+    else{
         setTimeout(() => {  console.log(x+" "+i); switchLight(i)}, x);
+    }
+    $(".light").css("background-color", "black");
     var id = i;
     $("#"+id).css("background-color", "yellow");
-    id=id-1
-    id=id%23
-    $("#"+id).css("background-color", "black");
+    // id=id-1
+    // id=id%23
+    // $("#"+id).css("background-color", "black");
 }
 var i = 0
 var x = 1000
